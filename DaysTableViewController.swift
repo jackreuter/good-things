3341b4600c5fb0ccc9081bf3096bf11e184a5376
Jack Reuter <jreuter@wesleyan.edu>
2014-12-24 16:52:45 -0500

    first commit

---
+++ DaysTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DaysTableViewController.swift
+//  GoodThings
+//
+//  Created by Jack Reuter on 12/24/14.
+//  Copyright (c) 2014 Jack Reuter. All rights reserved.
+//
+
+import Cocoa
+
+class DaysTableViewController: UITableViewController {
+
+}
